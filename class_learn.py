195c101ea90cf5d165b02b3f8065690e388dc49e
starsusu <a631840465@gmail.com>
2018-12-15 17:56:08 +0800

    #0 新建版本

---
+++ class_learn.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+#! usr/bin/python3
+# import xlrd
+class Animal(object):
+    def run(self):
+        print('Animal is running')
+        pass
+    pass
+
+class Dog(Animal):
+    def run(self):
+        print("Dog is running")
+
+class Cat(Animal):
+    def run(self):
+        print("Cat is running")
+
+
+class Tortoise(Animal):
+    def run(self):
+        print("Tortoise is running")
+
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+class Timer(object):
+    def run(self):
+        print("start..")
+
+if __name__ == '__main__':
+    run_twice(Tortoise())
+
+    # dog = Dog()
+    # dog.run()
+    #
+    # cat = Cat()
+    # cat.run()
+    
+
+
+
