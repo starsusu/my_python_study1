9a7629d3a72789804a33c4d785c7f3a3ba6d1b17
starsusu <a631840465@gmail.com>
2019-01-01 14:28:40 +0800

    #0 使用__slots__限制类的参数绑定，over

---
+++ class_high1.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+# ! usr/bin/python3
+
+from types import MethodType
+
+
+class Student(object):
+	# def __init__(self,name,age,score):
+	# 	self.__name = name
+	# 	self.__age = age
+	# 	self.__score = score
+
+	def set_age(self, age):
+		self.age = age
+		return self.age
+
+	# @property
+	# def get_score(self):
+	# 	return self.__score
+	#
+	# def get_age(self):
+	# 	return  self.__age
+	#
+	# def get_name(self):
+	# 	return self.__name
+	#
+	#
+	# def set_score(self,value):
+	# 	if not isinstance(value,int):
+	# 		raise ValueError('score must be an integer')
+	# 	if value < 0 or value > 100:
+	# 		raise ValueError('score must between 0~100')
+	# 	self.__score = value
+
+	__slots__ = ('name', 'age','score')
+
+
+
+s = Student()
+s.name = 'Michael'
+s.age = 29
+# s.score = 89
+
+# def set_age(self, age):
+# 	self.age = age
+# 	pass
+if __name__ == '__main__':
+	s2 = Student()
+	# s.set_age = MethodType(set_age,s)
+	# s2.set_age = MethodType(set_age,s2)
+
+	s.set_age(25)
+	print(s.age)
+	s2.set_age(26)
+	print(s2.age)
+	print(s.age)
